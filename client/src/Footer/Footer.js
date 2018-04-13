0af6bedb281de038d5b7a3248004ea90b0195700
Lawrence Anderson <anderson0741@icloud.com>
2018-04-13 17:25:06 -0600

    initial launch

--- client/src/Footer/Footer.js
+++ client/src/Footer/Footer.js
@@ -9,13 +9,13 @@ function Footer() {
         <div className="footerWrapper">
             <div className="footerBod">
                 <div className="icons">
-                    <a className='instagram' href="https://www.instagram.com/levelupcarsales/?hl=en"><img className='footerIcons' src={instagram} alt=""/></a>
-                    <a className='facebook' href="https://www.facebook.com/Level-Up-Car-Sales-513113469032684/?ref=page_internal"><img className='footerIcons' src={facebook} alt=""/></a>
+                    <a className='instagram' href="https://www.instagram.com/europeandobes/?hl=en"><img className='footerIcons' src={instagram} alt=""/></a>
+                    {/* <a className='facebook' href="https://www.facebook.com/Level-Up-Car-Sales-513113469032684/?ref=page_internal"><img className='footerIcons' src={facebook} alt=""/></a> */}
                 </div>
                 <div className="toPage">
-                <a className='home' href="/">Home</a>
+                {/* <a className='home' href="/">Home</a>
                 <p className='home'> / </p>
-                <a className='home' href="/cars">Shop</a>
+                <a className='home' href="/cars">Shop</a> */}
                 {/* <p> / </p> */}
                 {/* <a className='home' href="/login">Login</a> */}
             </div>
