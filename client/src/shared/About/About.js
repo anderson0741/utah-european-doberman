1909bb7dcdff41b22d63d5ea4d4d2b1f7befe02d
Lawrence Anderson <anderson0741@icloud.com>
2018-04-13 17:52:44 -0600

    initial launch

--- client/src/shared/About/About.js
+++ client/src/shared/About/About.js
@@ -5,15 +5,16 @@ import './About.css';
 function About(props) {
     return (
         <div>
-            <div>
-                <br />
-            </div>
-            <div className="theAbout">
-                <img className="aboutPic" src={theMom} alt="" />
-                <p></p>
-                <h1>"In 1972, a crack commando unit was sent to prison by a military court for a crime they didn't commit. These men promptly escaped from a maximum security stockade to the Los Angeles underground. Today, still wanted by the government they survive as soldiers of fortune. If you have a problem, if no one else can help, and if you can find them....maybe you can hire The A-Team."</h1>
-                <h1>"In 1972, a crack commando unit was sent to prison by a military court for a crime they didn't commit. These men promptly escaped from a maximum security stockade to the Los Angeles underground. Today, still wanted by the government they survive as soldiers of fortune. If you have a problem, if no one else can help, and if you can find them....maybe you can hire The A-Team."</h1>
-                <h1>"In 1972, a crack commando unit was sent to prison by a military court for a crime they didn't commit. These men promptly escaped from a maximum security stockade to the Los Angeles underground. Today, still wanted by the government they survive as soldiers of fortune. If you have a problem, if no one else can help, and if you can find them....maybe you can hire The A-Team."</h1>
+            <div className="theAboutWrapper">
+                <div className="theAboutTitle">
+                    <h1>About The Mom</h1>
+                </div>
+                <div className="theAbout">
+                    <img className="aboutPic" src={theMom} alt="" />
+                    <h1>"In 1972, a crack commando unit was sent to prison by a military court for a crime they didn't commit. These men promptly escaped from a maximum security stockade to the Los Angeles underground. Today, still wanted by the government they survive as soldiers of fortune. If you have a problem, if no one else can help, and if you can find them....maybe you can hire The A-Team."</h1>
+                    <h1>"In 1972, a crack commando unit was sent to prison by a military court for a crime they didn't commit. These men promptly escaped from a maximum security stockade to the Los Angeles underground. Today, still wanted by the government they survive as soldiers of fortune. If you have a problem, if no one else can help, and if you can find them....maybe you can hire The A-Team."</h1>
+                    <h1>"In 1972, a crack commando unit was sent to prison by a military court for a crime they didn't commit. These men promptly escaped from a maximum security stockade to the Los Angeles underground. Today, still wanted by the government they survive as soldiers of fortune. If you have a problem, if no one else can help, and if you can find them....maybe you can hire The A-Team."</h1>
+                </div>
             </div>
         </div>
     )
