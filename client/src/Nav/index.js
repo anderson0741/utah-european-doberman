4a99b763a908e726320e78b0b5de83cc52327503
Lawrence Anderson <anderson0741@icloud.com>
2018-04-13 16:30:19 -0600

    initial deploy

--- client/src/Nav/index.js
+++ client/src/Nav/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import images from '../images/level_up_cars.jpg';
+import images from '../images/Dobermanlogo2.jpg';
 import { connect } from "react-redux";
 import { logout } from "../redux/auth";
 // import Home from '../../shared/Home/index.js';
@@ -33,25 +33,6 @@ class Nav extends Component {
                             </div>
                         </div> : null}
                     </div>
-                    {/* <div className="navi "> */}
-                    {/* <img className='imgs' src={images} alt="" /> */}
-                    {/* <div className='links'><p>/</p></div>
-                        <div className='links'><Link className='links' to="/">Home</Link></div>
-                        <div className='links'><p>/</p></div>
-                        <div className='links'><Link className='links' to="/about">About</Link></div>
-                        <div className='links'><p>/</p></div>
-                        <div className='links'><Link className='links' to="/cars">Shop Cars</Link></div>
-                        <div className='links'><p>/</p></div>
-                        {isAuthenticated ? <div className='links'><Link className='links' to="/upload">Upload Content</Link></div> : null} */}
-                    {/* <div className='links'><Link className='links' to="/upload">Upload Content</Link></div> */}
-                    {/* {isAuthenticated ? <div className='links'><p>/</p></div> : null} */}
-                    {/* <div className='links'><Link className='links' to="/contact">Contact Us</Link></div> */}
-                    {/* <Link to="/image">Upload Image</Link> */}
-                    {/* {isAuthenticated ? <div className='links'><p>/</p></div> : null}
-                        {isAuthenticated ? <div className="links"><a className='links' onClick={this.props.logout}>Logout</a></div> : null}
-                        <div className='links'><p>/</p></div>
-                        {isAuthenticated ? null : <div className='links'><Link className='links' to="/login">Login</Link></div>} */}
-                    {/* </div> */}
                 </div>
             </div>
         )
