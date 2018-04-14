55aea17aee3fb7918ae5d0b5af0047353f63a7d8
Lawrence Anderson <anderson0741@icloud.com>
2018-04-13 19:44:07 -0600

    initial launch

--- client/src/shared/Contact/index.js
+++ client/src/shared/Contact/index.js
@@ -4,52 +4,54 @@ import './Contact.css';
 function Contact() {
     return (
         <div class="addresses">
-            <div className="deets">
-                <div className="surveyTitle">
-                    <h1 className='detailSurvey'>Reserve your European Doberman</h1>
-                    <p className='detailSurvey'>Dogs are 2,000 - 3,200 based on options selected. </p>
-                    <h3 className='detailSurvey'>Email: Europeandobes@gmail.com</h3>
-                </div>
-                <div className="infoSurvey">
-                    <h3 className="surveyInputs">Email:</h3>
-                    <input type="text" placeholder='Email' />
-                    <h3 className="surveyInputs">Location:</h3>
-                    <input type="text" placeholder='Where are you located?' />
-                    <h3 className="surveyInputs">Phone Number:</h3>
-                    <input type="text" placeholder='Phone Number' />
-                    <h3 className="surveyInputs">Social Media:</h3>
-                    <input type="text" placeholder='Instagram/Facebook handle' />
-                </div>
-                <div className="contactSurvey">
-                    <h3 className="surveyInputs">Best way to contact:</h3>
-                    <label htmlFor=""><input type="radio" name="contactInfo" value="Phone" /> Phone</label>
-                    <label htmlFor=""><input type="radio" name="contactInfo" value="Email" /> Email</label>
-                    <label htmlFor=""><input type="radio" name="contactInfo" value="Social Media" /> Social Media</label>
-                </div>
-                <div className="genderSurvey">
-                    <h3 className="surveyInputs">Preferred Gender:</h3>
-                    <label htmlFor=""><input type="checkbox" name="contactInfo" value="Male" /> Male</label>
-                    <label htmlFor=""><input type="checkbox" name="contactInfo" value="Female" /> Female</label>
-                    <label htmlFor=""><input type="checkbox" name="contactInfo" value="none" /> No Preference</label>
-                </div>
-                <div className="breedSurvey">
-                    <h3 className="surveyInputs">Do you plan to breed:</h3>
-                    <label htmlFor=""><input type="radio" name="contactInfo" value="Yes" /> Yes</label>
-                    <label htmlFor=""><input type="radio" name="contactInfo" value="No" /> No</label>
-                </div>
-                <div className="appearanceSurvey">
-                    <h3 className="surveyInputs">Preference on appearance:</h3>
-                    <div className="optionSurvey">
-                        <label htmlFor=""><input type="checkbox" name="contactInfo" value="Ears" /> Ears cropped (dogs ears are cropped 9-12 weeks old)</label>
-                        <label htmlFor=""><input type="checkbox" name="contactInfo" value="Tail" /> Tail Docked</label>
+            <form name="contact" method="POST">
+                <div className="deets">
+                    <div className="surveyTitle">
+                        <h1 className='detailSurvey'>Reserve your European Doberman</h1>
+                        <p className='detailSurvey'>Dogs are 2,000 - 3,200 based on options selected. </p>
+                        <h3 className='detailSurvey'>Email: Europeandobes@gmail.com</h3>
                     </div>
+                    <div className="infoSurvey">
+                        <h3 className="surveyInputs">Email:</h3>
+                        <input type="text" placeholder='Email' />
+                        <h3 className="surveyInputs">Location:</h3>
+                        <input type="text" placeholder='Where are you located?' />
+                        <h3 className="surveyInputs">Phone Number:</h3>
+                        <input type="text" placeholder='Phone Number' />
+                        <h3 className="surveyInputs">Social Media:</h3>
+                        <input type="text" placeholder='Instagram/Facebook handle' />
+                    </div>
+                    <div className="contactSurvey">
+                        <h3 className="surveyInputs">Best way to contact:</h3>
+                        <label htmlFor=""><input type="radio" name="contactInfo" value="Phone" /> Phone</label>
+                        <label htmlFor=""><input type="radio" name="contactInfo" value="Email" /> Email</label>
+                        <label htmlFor=""><input type="radio" name="contactInfo" value="Social Media" /> Social Media</label>
+                    </div>
+                    <div className="genderSurvey">
+                        <h3 className="surveyInputs">Preferred Gender:</h3>
+                        <label htmlFor=""><input type="checkbox" name="contactInfo" value="Male" /> Male</label>
+                        <label htmlFor=""><input type="checkbox" name="contactInfo" value="Female" /> Female</label>
+                        <label htmlFor=""><input type="checkbox" name="contactInfo" value="none" /> No Preference</label>
+                    </div>
+                    <div className="breedSurvey">
+                        <h3 className="surveyInputs">Do you plan to breed:</h3>
+                        <label htmlFor=""><input type="radio" name="contactInfo" value="Yes" /> Yes</label>
+                        <label htmlFor=""><input type="radio" name="contactInfo" value="No" /> No</label>
+                    </div>
+                    <div className="appearanceSurvey">
+                        <h3 className="surveyInputs">Preference on appearance:</h3>
+                        <div className="optionSurvey">
+                            <label htmlFor=""><input type="checkbox" name="contactInfo" value="Ears" /> Ears cropped (dogs ears are cropped 9-12 weeks old)</label>
+                            <label htmlFor=""><input type="checkbox" name="contactInfo" value="Tail" /> Tail Docked</label>
+                        </div>
+                    </div>
+                    <br />
+                    <div className="submitSurvey">
+                        <input className="submit" type="submit" value="Submit" />
+                    </div>
+                    <br />
                 </div>
-                <br/>
-                <div className="submitSurvey">
-                    <input className="submit" type="submit" value="Submit" />
-                </div>
-                <br/>
-            </div>
+            </form>
             {/* <button className="submit" onClick={this.props.toggleEdit}>Cancel</button> */}
         </div>
     )
