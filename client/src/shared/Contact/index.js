eb35bef73707498f889a4384cfbc27be409e1518
Lawrence Anderson <anderson0741@icloud.com>
2018-04-13 18:58:31 -0600

    initial launch

--- client/src/shared/Contact/index.js
+++ client/src/shared/Contact/index.js
@@ -7,9 +7,41 @@ function Contact() {
             <div className="deets">
                 <h3>Email: Europeandobes@gmail.com</h3>
                 <h3>Tel: 555-555-5555</h3>
+                {/* </div>
+            <div className="survey"> */}
+                <p className="surveyInputs">Email:
+                    <input type="text" placeholder='Email' />
+                </p>
+                <p className="surveyInputs">Location:
+                    <input type="text" placeholder='Where are you located?' />
+                </p>
+                <p className="surveyInputs">Phone Number:
+                    <input type="text" placeholder='Phone Number' />
+                </p>
+                <p className="surveyInputs">Social Media
+                    <input type="text" placeholder='Instagram/Facebook handle' />
+                </p>
+                <p className="surveyInputs">Best way to contact:
+                    <label htmlFor=""><input type="radio" name="contactInfo" value="Phone" /> Phone</label>
+                    <label htmlFor=""><input type="radio" name="contactInfo" value="Email" /> Email</label>
+                    <label htmlFor=""><input type="radio" name="contactInfo" value="Social Media" /> Social Media</label>
+                </p>
+                <p className="surveyInputs">Preferred Gender:
+                    <label htmlFor=""><input type="checkbox" name="contactInfo" value="Male" /> Male</label>
+                    <label htmlFor=""><input type="checkbox" name="contactInfo" value="Female" /> Female</label>
+                    <label htmlFor=""><input type="checkbox" name="contactInfo" value="none" /> No Preference</label>
+                </p>
+                <p className="surveyInputs">Do you plan to breed:
+                    <label htmlFor=""><input type="radio" name="contactInfo" value="Yes" /> Yes</label>
+                    <label htmlFor=""><input type="radio" name="contactInfo" value="No" /> No</label>
+                </p>
+                <p className="surveyInputs">Preference on appearance:
+                    <label htmlFor=""><input type="checkbox" name="contactInfo" value="Ears" /> Ears cropped (dogs ears are cropped 9-12 weeks old)</label>
+                    <label htmlFor=""><input type="checkbox" name="contactInfo" value="Tail" /> Tail Docked</label>
+                </p>
+                <input className="submit" type="submit" value="Submit" />
             </div>
-            <br />
-            <iframe className="embbAddress" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3003.716473453886!2d-111.94432221965292!3d41.16253767938688!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x875306723e831743%3A0xe656c6d6b76381b1!2s1490+E+5600+S%2C+Ogden%2C+UT+84403!5e0!3m2!1sen!2sus!4v1513742225623">Address</iframe>
+            {/* <button className="submit" onClick={this.props.toggleEdit}>Cancel</button> */}
         </div>
     )
 }
