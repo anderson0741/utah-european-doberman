981556678c9dc4a6d4aa0af6ef19b5f406a6afac
Lawrence Anderson <anderson0741@icloud.com>
2018-04-17 19:20:02 -0600

    updates

--- client/src/shared/Content/Form/Guest/GuestDisplay.js
+++ client/src/shared/Content/Form/Guest/GuestDisplay.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import GuestListing from './GuestListing';
-import loading from '../../../../images/loading.gif'
+import loading from './loading.gif';
+// import loading from '../../../../images/farm.jpg';
 
 export default class GuestDisplay extends Component {
     render() {
         let { listings, loading } = this.props;
         return (
             loading ?
-                <div>
-                    {loading}
+                <div className="loadingscene">
+                    {/* {loading} */}
+                    {/* loading */}
+                    {/* <img src={loading} alt="loading"/> */}
                 </div>
                 :
                 <div>
