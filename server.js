4e9287e6847d67327be004ed264bce74ab8413e6
Lawrence Anderson <anderson0741@icloud.com>
2018-04-25 16:02:06 -0600

    server repair

--- server.js
+++ server.js
@@ -17,10 +17,10 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/listings", (err
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "client", "build")))
-app.use("/api/public/listing", require('./routes/public'));
-// app.use("/listing/", expressJwt({secret: process.env.SECRET}));
-app.use("/auth", require("./routes/auth"));
-app.use('/api/listing', require('./routes/routes'));
+// app.use("/api/public/listing", require('./routes/public'));
+app.use("/api/listing/", expressJwt({secret: process.env.SECRET}));
+// app.use("/auth", require("./routes/auth"));
+app.use('/listing', require('./routes/routes'));
 app.use('/api/upload', require('./routes/routes'));
 
 app.get("*", (req, res) => {  
