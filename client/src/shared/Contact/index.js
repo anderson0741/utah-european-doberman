f217636ba9eec63fccc33d9fd2281ec8b84d3629
Lawrence Anderson <anderson0741@icloud.com>
2018-05-21 00:10:22 -0600

    css

--- client/src/shared/Contact/index.js
+++ client/src/shared/Contact/index.js
@@ -4,11 +4,13 @@ import './Contact.css';
 function Contact() {
     return (
         <div className="contactad">
-            <div className="surveyTitle">
+            <div className="contactTitle">
                 <h1 className='detailSurvey'>Reserve your European Doberman</h1>
+                <h2 className='detailSurvey'>Vernal, UT</h2>
+                <h2 className='detailSurvey'>United States</h2>
                 {/* <p className='detailSurvey'>Dogs are 2,000 - 3,200 based on options selected. </p> */}
-                <h3 className='detailSurvey'>Email: Europeandobes@gmail.com</h3>
                 <h3 className='detailSurvey'>Phone: (801) 674-2062</h3>
+                <h3 className='detailSurvey'>Email: Europeandobes@gmail.com</h3>
             </div>
         </div>
     )
