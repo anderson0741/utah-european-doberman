a4a97a1c2379bc36a6b458c284701c15ea20b71c
Lawrence Anderson <anderson0741@icloud.com>
2018-05-21 00:03:59 -0600

    phone

--- client/src/shared/Contact/index.js
+++ client/src/shared/Contact/index.js
@@ -6,8 +6,9 @@ function Contact() {
         <div className="contactad">
             <div className="surveyTitle">
                 <h1 className='detailSurvey'>Reserve your European Doberman</h1>
-                <p className='detailSurvey'>Dogs are 2,000 - 3,200 based on options selected. </p>
+                {/* <p className='detailSurvey'>Dogs are 2,000 - 3,200 based on options selected. </p> */}
                 <h3 className='detailSurvey'>Email: Europeandobes@gmail.com</h3>
+                <h3 className='detailSurvey'>Phone: (801) 674-2062</h3>
             </div>
         </div>
     )
